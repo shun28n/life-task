abb73b90e899a563dbffcdad301838f6ae3a30e5
shun_n <efsf-rx78-gp02a-1@ezweb.ne.jp>
2022-04-23 23:56:28 +0900

    checkList-edit fix

--- src/components/uiParts/CheckListEdit.vue
+++ src/components/uiParts/CheckListEdit.vue
@@ -3,28 +3,35 @@
     <v-row width="100%">
       <v-col
         cols="12"
-        v-for="index in checks.length"
+        v-for="(check, index) in checks"
         :key="index"
         class="pt-0 d-flex flex-row"
         width="100%"
-        @keyup.enter="addCheck"
+        @keyup.enter="onEditCheckList"
       >
-        <!-- textfieldのアイコンイベント使った方が良さそう -->
-        <v-checkbox
-          class="check-list"
-          color="grey darken-1"
-          :input-value="checks[index - 1].value"
-        >
-        </v-checkbox>
-
-        <!-- 入力値をdataにもつ -->
+        <!-- フォーカスイベントでチェックリスト変更 -->
         <v-text-field
-          v-model="checks[index - 1].body"
-          :class="{ 'text-decoration-line-through': checks[index - 1].value }"
-          @blur="onBlurCheckForm(checks[index - 1].id)"
+          v-model="checks[index].body"
+          :class="{ 'text-line-through': checks[index].value }"
+          @blur="onEditCheckList(checks[index].id)"
           dense
-          clearable
         >
+          <template v-slot:prepend>
+            <v-icon
+              v-if="check.value"
+              @click="onCheck(index)"
+              v-bind="check.value"
+            >
+              mdi-checkbox-marked
+            </v-icon>
+            <v-icon v-else @click="onCheck(index)" v-bind="check.value">
+              mdi-checkbox-blank-outline
+            </v-icon>
+          </template>
+
+          <template v-slot:append>
+            <v-icon @click="onDelete(index)"> mdi-delete </v-icon>
+          </template>
         </v-text-field>
       </v-col>
     </v-row>
@@ -53,55 +60,74 @@ export default {
     // checksの値に変更があればフィールドを１つ追加したい
   },
   methods: {
-    addCheck() {
-      console.log("addCheck入りました", this.checks);
-      let newCheckList = {
-        body: this.body,
-        value: false,
-      };
-      this.checks.push(newCheckList);
-      this.body = "";
-    },
-    onBlurCheckForm(checkId) {
-      console.log("onBlur:checkId->" + checkId);
-      console.log(this.checks);
+    // チェックリスト値変更, フォーム追加
+    onEditCheckList() {
       let payload = {
         taskId: this.taskId,
-        checks: [
-          { id: 1, body: "a", value: false },
-          { id: 2, body: "b", value: false },
-        ],
+        checks: this.checks,
       };
       this.$emit("change", payload);
+      this.addNewForm();
+    },
+
+    // 要素番号からチェックリスト削除
+    onDelete(index) {
+      if (this.checks[index].body !== "") {
+        this.checks.splice(index, 1);
+        let payload = {
+          taskId: this.taskId,
+          checks: this.checks,
+        };
+        this.$emit("change", payload);
+        this.addNewForm();
+      }
+    },
+
+    // 新チェックリストの追加  **enterからの場合新しいチェックリストにフォーカス当てたい
+    addNewForm() {
+      console.log("addNewFormです");
+      // 空のチェックリストに割り当てるID作成
+      let max = 0;
+      for (let i = 0; i < this.checks.length; i++) {
+        //登録済チェックリストから最大のIDを取得
+        let checkId = this.checks[i].id;
+        if (max < checkId) {
+          max = checkId;
+        }
+      }
+
+      let newList = {
+        id: max + 1,
+        body: "",
+        value: false,
+      };
+      // チェックリストが空か、登録済の場合＋１(無限増幅回避)
+      const checksSize = this.checks.length;
+      if (checksSize === 0 || this.checks[checksSize - 1].body !== "") {
+        this.checks.push(newList);
+      }
+    },
+    onCheck(index) {
+      if (this.checks[index].body !== "") {
+        if (this.checks[index].value === true) {
+          this.checks[index].value = false;
+        } else {
+          this.checks[index].value = true;
+        }
+      }
     },
   },
-  //チェックリスト表示
+  //空チェックリスト表示
   created() {
-    // 空のチェックリストに割り当てるID作成
-    let max = 0;
-    for (let i = 0; i < this.checks.length; i++) {
-      //登録済チェックリストから最大のIDを取得
-      let checkId = this.checks[i].id;
-      if (max < checkId) {
-        max = checkId;
-      }
-    }
-
-    let newList = {
-      id: max + 1,
-      body: "",
-      value: false,
-    };
-    // チェックリストが空か、登録済の場合＋１(無限増幅回避)
-    const checksSize = this.checks.length;
-    if (checksSize === 0 || this.checks[checksSize - 1].body !== "") {
-      this.checks.push(newList);
-    }
+    this.addNewForm();
   },
 };
 </script>
 
 <style>
+.text-line-through input {
+  text-decoration-line: line-through;
+}
 /* >>> .v-messages {
   min-height: 0px !important;
 } */
